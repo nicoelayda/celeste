2328fa01c9394cc57b14f7d31025e390ac7c9182
Rohit Malyala <malyalar@Rohits-MacBook-Pro.local>
2022-11-28 20:47:25 -0800

    reorganized locations of dependencies

---
+++ examples/main.js
@@ -0,0 +1,5 @@
+var stereoComponent = require('../index.js').stereo_component;
+var stereocamComponent = require('../index.js').stereocam_component;
+
+AFRAME.registerComponent('stereo', stereoComponent);
+AFRAME.registerComponent('stereocam', stereocamComponent);
